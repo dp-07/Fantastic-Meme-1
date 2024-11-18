24982b80351b1d9426df83da8c5d5e76e8d4e16c
Paras <w17.paras@gmail.com>
2024-11-18 23:07:46 +0530

    First commit

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 console.log("Hello World!!!");
+console.alert("HI THERE!!!");
+
+    var name = prompt("What is your name?");
+    console.log("Hello " + name);
+
+    var age = prompt("How old are you?");
+    console.log("You are " + age + " years old");
+
+    var height = prompt("How tall are you?");
+    console.log("You are " + height + " centimeters tall");
+
+    var weight = prompt("How much do you weight?");
+    console.log("You weigh " + weight + " kilograms");
+
+    var gender = prompt("What is your gender?");
+    console.log("You are " + gender);
+
+    var hobby = prompt("What is your hobby?");
+    console.log("Your hobby is " + hobby);
+
+    var food = prompt("What is your favourite food?");
+    console.log("Your favourite food is " + food);
