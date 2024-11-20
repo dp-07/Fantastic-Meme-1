b710788cb4ab444b4ca2b4c55468dc729791577e
= <=>
2024-11-20 21:26:59 +0530

    First react page.

---
+++ react_app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey guys
+          </p>
+          <p> Welcome to Paras and Hariom's world.
+          Great things are cooking!.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
